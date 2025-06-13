8393c2b5fefa0831a507b0577ba5f669f387df23
Adri Arcones <adrianarconesgomez@gmail.com>
2025-06-13 15:06:28 +0200

    feat: project structure && vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Escucha en todas las interfaces
+    port: 5173,
+    strictPort: true, 
+    open: ['http://localhost:5173', 'http://192.168.1.42:5173'] 
+  }
 })
